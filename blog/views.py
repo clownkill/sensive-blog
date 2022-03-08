540e442359a8dc5857d64b6dd4e5a2573529440a
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-03-08 10:57:10 +0300

    show popular posts on main page

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,11 @@ def serialize_tag(tag):
 
 def index(request):
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = sorted(
+        Post.objects.all(),
+        key=get_likes_count,
+        reverse=True
+    )[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
@@ -117,3 +121,7 @@ def contacts(request):
     # позже здесь будет код для статистики заходов на эту страницу
     # и для записи фидбека
     return render(request, 'contacts.html', {})
+
+
+def get_likes_count(post):
+    return post.likes.count()
