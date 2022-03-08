8e7eee06ca1343e2ad99336242a216a9a91f1f02
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-03-08 17:58:46 +0300

    comments count with post_comments

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,7 @@ def serialize_post_optimized(post):
         'title': post.title,
         'teaser_text': post.text[:200],
         'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
+        'comments_amount': post.comments.all().count(),
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
