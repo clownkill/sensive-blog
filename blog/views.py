9fb265f535289cc8cfce5040e7bfd49287feb51d
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-03-08 11:25:45 +0300

    optimize popular posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
+from django.db.models import Count
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
+
+
+def get_likes_count(post):
+    return post.likes__count
 
 
 def get_related_posts_count(tag):
@@ -29,7 +34,7 @@ def serialize_tag(tag):
 
 def index(request):
     most_popular_posts = sorted(
-        Post.objects.all(),
+        Post.objects.annotate(Count('likes')),
         key=get_likes_count,
         reverse=True
     )[:5]
@@ -120,7 +125,3 @@ def contacts(request):
     # позже здесь будет код для статистики заходов на эту страницу
     # и для записи фидбека
     return render(request, 'contacts.html', {})
-
-
-def get_likes_count(post):
-    return post.likes.count()
