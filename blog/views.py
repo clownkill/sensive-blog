977aed74900532d097e03d4f52eb133ba21ea536
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-03-10 18:18:30 +0300

    Revert "reduce db queries number"
    This reverts commit 027ef2263b3bb8f7b7586012a8328a777964377e.

--- blog/views.py
+++ blog/views.py
@@ -35,7 +35,7 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in Tag.objects.popular()],
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -49,7 +49,7 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in Tag.objects.popular()],
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -57,7 +57,7 @@ def serialize_post(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': tag.posts_amount,
+        'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
 
@@ -90,7 +90,7 @@ def post_detail(request, slug):
 
     likes = post.likes.all()
 
-    related_tags = Tag.objects.popular()
+    related_tags = post.tags.all()
 
     serialized_post = {
         'title': post.title,
